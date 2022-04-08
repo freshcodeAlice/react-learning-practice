60944345312a04287cc2da890ce6ed71e51e5251
freshcodeAlice <freshjs.ofte@gmail.com>
2022-04-08 17:20:39 +0300

    First stage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import FormContainer from './components/FormContainer';
 
 function App() {
   return (
-  
+  <FormContainer />
   );
 }
 
---
+++ src/components/FormContainer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class FormContainer extends React.Component {
+
+    render() {
+
+
+        return (
+            <section className='form-container'>
+
+            </section>
+        )
+    }
+}
+
+
+export default FormContainer;
