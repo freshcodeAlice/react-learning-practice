4171a787f03c929df4e64060bb1578c56be5daf2
freshcodeAlice <freshjs.ofte@gmail.com>
2022-04-14 17:37:34 +0300

    Prettier-Standard package

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import './App.css';
 import React from 'react';
 import Layout from './components/Layout';
 
-
-
-function App(props) {
-  return (
-    <Layout />
-      );
+function App (props) {
+  return <Layout />;
 }
 
 export default App;
