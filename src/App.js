33af57927639b1303e9ba8b2761757bbaa4ba87f
freshcodeAlice <freshjs.ofte@gmail.com>
2022-04-18 18:26:53 +0300

    window api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import './App.css';
 import React from 'react';
-import UsersLoader from './components/UsersLoader';
 
 function App (props) {
-  return <UsersLoader />;
+  return (
+    <>
+      <p>
+        <button
+          onClick={() => {
+            window.close();
+          }}
+        >
+          Close
+        </button>
+        <button
+          onClick={() => {
+            window.open('https://google.com');
+          }}
+        >
+          Open
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            window.location.assign(
+              'https://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8'
+            );
+          }}
+        >
+          Assign
+        </button>
+        <button
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          Reload
+        </button>
+        <button
+          onClick={() => {
+            window.location.replace('https://google.com');
+          }}
+        >
+          Replace
+        </button>
+      </p>
+    </>
+  );
 }
 
 export default App;
