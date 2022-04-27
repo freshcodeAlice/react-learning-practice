fa5dad0d5edca3bee4aced62b968c17c516782a8
freshcodeAlice <freshjs.ofte@gmail.com>
2022-04-27 19:27:24 +0300

    useField

--- src/components/SignUpForm/Input/index.js
+++ src/components/SignUpForm/Input/index.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { Field, ErrorMessage } from 'formik';
+import { Field, ErrorMessage, useField } from 'formik';
 import styles from './Input.module.css';
 import cx from 'classnames';
 
 const Input = ({ name, ...rest }) => {
+  const [field, meta, helpers] = useField(name);
+
+  const classNames = cx(styles.input, {
+    [styles.validInput]: meta.touched && !meta.error,
+    [styles.invalidInput]: meta.touched && meta.error,
+  });
   return (
     <label>
-      <Field name={name}>
-        {({ field, form, meta }) => {
-          console.log(meta);
-          const classNames = cx(styles.input, {
-            [styles.validInput]: meta.touched && !meta.error,
-            [styles.invalidInput]: meta.touched && meta.error,
-          });
-          return (
-            <input type='text' {...field} className={classNames} {...rest} />
-          );
-        }}
-      </Field>
+      <input type='text' {...field} className={classNames} {...rest} />
       <ErrorMessage name={name} />
     </label>
   );
